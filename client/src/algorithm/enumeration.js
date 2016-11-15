03e7c97403434eab8088e7b206b1cebc3d709464
yinglong.xue <yinglong.xue@timanetworks.com>
2016-11-15 10:10:48 +0800

    1.算法，深度优先搜索，全排列

--- client/src/algorithm/enumeration.js
+++ client/src/algorithm/enumeration.js
@@ -94,31 +94,62 @@ function playMatch(m) {
     console.log(sum);
 }
 //playMatch(18);
+
+//深度优先搜索
 var total = 0;
-var a= new Array(10);
+var a= new Array(9);
 var book = {};
 //当前桌面上的牌出现的次数，0表示没出现过，1表示已经出现
-for(var i = 0; i < 10; i++){
+for(var i = 0; i < 9; i++){
     book[i] = 0;
 }
 
-function dfs(step) {
-    if(step == 10){
+//全排列
+var n = 3;
+function dfs(step){
+    "use strict";
+    if(step == n){ //如果站在第n+1个盒子面前，则表示前n个盒子已经放好扑克牌
+        //输出一种排列
+        total++;
+       /* for(var i = 0; i < n; i++){
+            console.log(a[i]);
+        }*/
+        return;//返回之前的一步（最近一次调用dfs函数的地方）
+    }
+    //此时站在第step个盒子面前，应该放哪张牌呢？
+    //按照1,2,3....n的顺序一一尝试
+    for(var i = 0; i < n; i++){
+        if(book[i] == 0){ //book[i]等于0表示i号牌在手中
+            //开始尝试使用扑克牌
+            a[step] = i;
+            book[i] = 1; //将book[i]设为1，表示i号扑克牌已经不在手上
+
+            //第step个盒子已经放好扑克牌，接下来需要走到下一个盒子面前
+            dfs(step + 1);
+            book[i] = 0;//一定要将刚才尝试的扑克牌收回，才能进行下一次尝试
+        }
+    }
+    return;
+}
+
+/*function dfs(step) {
+    if(step == 10){//判断边界条件
         if(a[0]*100 + a[2]*10 + a[3] + a[4]*100 + a[5]*10 + a[6] == a[7]*100 + a[8]*10 + a[9]){
             total++;
             console.log(a[0] + '----' + a[1] + '----'+ a[2] + '----'+ a[3] + '----'+ a[4] + '----'+ a[5] + '----'+ a[6] + '----'+ a[7] + '----'+ a[8] + '----'+ a[9] + '----')
         }
         return;
     }
-    for(var i = 0; i < 9; i++){
+    for(var i = 1; i <= 9; i++){
         if(book[i] == 0){
             a[step] = i;
             book[i] = 1;
-            dfs(step+1);
+            dfs(step+1); //递归调用
             book[i] = 0;
         }
     }
     return;
-}
+}*/
 dfs(0);
+console.log(total);
 
