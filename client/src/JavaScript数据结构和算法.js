e34156640900bc88b011abab2b09b9c696b23c04
TurboLoong <xue.yinglong@linkingmed.com>
2017-01-20 09:59:11 +0800

    1.数据结构

--- client/src/JavaScript数据结构和算法.js
+++ client/src/JavaScript数据结构和算法.js
@@ -161,6 +161,26 @@ function LinkedList() {
 
 //集合{value:value}
 
+//字典
+function Dictionary() {
+  var items = {};
+  this.has = function (key) {
+    return key in items;
+  }
+  this.set = function (key, value) {
+    items[key] = value;
+  }
+  this.remove = function (key) {
+    if(items.has[key]){
+      delete items[key];
+    }
+  }
+  this.get = function (key) {
+    if(items.has[key]){
+      return items[key];
+    }
+  }
+}
 //散列表
 var ValuePair = function (key, value) {
   this.key = key;
@@ -182,5 +202,148 @@ function HashTable() {
     }
     table[position].append(new ValuePair(key, value));
   }
+}
 
+//树
+function BinarySearchTree() {
+  var Node = function (key) {
+    this.key = key;
+    this.left = null;
+    this.right = null;
+  };
+  var root = null;
+  var insertNode = function (node, newNode) {
+    if(newNode.key<node.key){
+      if(node.left === null){
+        node.left = newNode;
+      }else{
+        insertNode(node.left, newNode);
+      }
+    }else{
+      if(node.right === null){
+        node.right = newNode;
+      }else{
+        insertNode(node.right, newNode);
+      }
+    }
+  }
+  var inOrderTraverseNode = function (node, callback) {
+    if(node !== null){
+      inOrderTraverseNode(node.left, callback);
+      callback(node.key);
+      inOrderTraverseNode(node.right, callback);
+    }
+  }
+  var preOrderTraverseNode = function (node, callback) {
+    if(node !== null){
+      callback(node,key);
+      preOrderTraverseNode(node.left, callback);
+      preOrderTraverseNode(node.right, callback);
+    }
+  }
+  var postOrderTraverseNode = function (node, callback) {
+    if(node.key !== null){
+      postOrderTraverseNode(node.left, callback);
+      postOrderTraverseNode(node.right, callback);
+      callback(node, key);
+    }
+  }
+  var minNode = function (node) {
+    if(node){
+      while(node&&node.left !== null){
+        node = node.left;
+      }
+      return node.key;
+    }
+    return null;
+  }
+  var maxNode = function (node) {
+    if(node){
+      while(node&&node.right){
+        node = node.right;
+      }
+      return node.key;
+    }
+    return null;
+  }
+  var searchNode = function (node, key) {
+    if(node === null){
+      return false;
+    }
+    if(key<node.key){
+      searchNode(node.left, key);
+    }else if(key > node.key){
+      searchNode(node.right, key);
+    }else{
+      return true;
+    }
+  }
+  var removeNode = function (node, key) {
+    if(node === null){
+      return null;
+    }
+    if(key < node.key){
+      node.left = removeNode(node.left, key);
+      return node;
+    }else if(key > node.key){
+      node.right = removeNode(node.right, key);
+    }else{
+      if(node.left === null && node.right === null){
+        node == null;
+        return node;
+      }
+      if(node.left === null){
+        node = node.right;
+        return node;
+      }else if(node.right === null){
+        node = node.left;
+        return node;
+      }
+
+    }
+  }
+  this.insert = function (key) {
+    var newNode = new Node(key);
+    if(root === null){
+      root = newNode;
+    }else{
+      insertNode(root, newNode);
+    }
+  }
+  //中序遍历
+  this.inOrderTraverse = function (callback) {
+    inOrderTraverseNode(root, callback);
+  }
+  //先序遍历
+  this.preOrderTraverse = function (callback) {
+    preOrderTraverseNode(root, callback);
+  }
+  //后序遍历
+  this.postOrderTraverse = function (callback) {
+    postOrderTraverseNode(root, callback);
+  }
+  this.minNode = function () {
+    return minNode(root);
+  }
+  this.maxNode = function () {
+    return maxNode(root);
+  }
+  this.search = function (key) {
+    return searchNode(root, key);
+  }
+  this.removeNode = function (key) {
+    root = removeNode(root, key);
+  }
+}
+
+function Graph() {
+  var vertices = [];
+  var adjList = new Dictionary();
+  this.addVertex = function (v) {
+    vertices.push(v);
+    adjList.set(v, []);
+  }
+  this.addEdge = function (v, w) {
+    adjList.get(v).push(w);
+  }
 }
--- client/src/helloworld.js
+++ client/src/helloworld.js
@@ -28,3 +28,4 @@ function setupModuleLoader(obj) {
         }
     })
 }
+$('#datetimepicker').datetimepicker();
