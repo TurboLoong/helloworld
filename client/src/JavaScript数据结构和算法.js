22475019f8d52aa0aae2f92337ce7ef55ec085e1
TurboLoong <xue.yinglong@linkingmed.com>
2017-02-07 16:09:50 +0800

    1.数据结构

--- client/src/JavaScript数据结构和算法.js
+++ client/src/JavaScript数据结构和算法.js
@@ -336,61 +336,113 @@ function BinarySearchTree() {
     root = removeNode(root, key);
   }
 }
-
+//图的表示
+//邻接矩阵，邻接表，关联矩阵
 function Graph() {
-  var vertices = [];
-  var adjList = new Dictionary();
+  var vertices = []; //顶点
+  var adjList = new Dictionary(); //用字典来存储邻接表
+  //添加定点
+  this.addVertex = function (v) {
+    vertices.push(v);
+    adjList.set(v, []);
+  }
+  //添加顶点之间的边（无向图）
+  this.addEdge = function (v, w) {
+    adjList.get(v).push(w);
+    adjList.get(w).push(v);//如果是有向图，只需要添加一个顶点的边，及此行不要
+  }
+  //图的遍历，广度优先，深度优先
+  //图遍历算法的思想是必须追踪每个第一次访问的节点，并且追踪有哪些节点还没有被完全探索
+  //务必访问每个顶点至多两次。
+  /**
+   *@author: Turbo Loong
+   *@date: 2017/1/20
+   *@disc: 广度优先搜索
+   * (1)创建一个队列
+   * (2)将v标注为被发现的,并将v入队列Q
+   * (3)如果Q非空，则运行以下步骤：
+   *  (a)将u从Q中出队列
+   *  (b)将标注u为被发现的
+   *  (c)将u所有未被访问过的邻点入队列
+   *  (d)将u标注为已被探索的
+   * 总结：入队，出队，置为黑
+   */
   var initializeColor = function () {
     var color = {};
     for(var i=0; i<vertices.length;i++){
       color[vertices[i]] = 'white';
     }
     return color;
-}
-  this.addVertex = function (v) {
-    vertices.push(v);
-    adjList.set(v, []);
   }
-  this.addEdge = function (v, w) {
-    adjList.get(v).push(w);
-  }
-  /**
-   *@author: Turbo Loong
-   *@date: 2017/1/20
-   *@disc: 广度优先搜索
-   * (1)创建一个队列
-   * (2)将v标注为被发现的,并将v如队列Q
-   * (3)如果Q非空，则运行以下步骤：
-   *  (a)将u从Q中出队列
-   *  (b)将标注u为被发现的
-   *  (c)将u所有未被访问过的邻点入队列
-   *  (d)将u标注为已被探索的
-   */
   this.bfs = function (v, callback) {
     //初始化
     var color = initializeColor(),
       queue = new Queue();
-    //定点入队
+    //起始顶点入队
     queue.enqueue(v);
     //如果队列不为空
     while(!queue.isEmpty()){
       //出队，获取邻居
       var u = queue.dequeue(),
         neighbors = adjList.get(u);
-      color[u] = 'grey';
-      //遍历邻居
+      color[u] = 'grey'; //发现了顶点，但还未完成对其的探索
+      //遍历邻点
       for(var i = 0; i < neighbors.length; i++){
         var w = neighbors[i];
-        //设为灰色，入队
+        //如果还未被访问到，设为灰色，入队
         if(color[w] == 'white'){
           color[w] = 'grey';
           queue.enqueue(w);
         }
       }
-      color[u] = 'black';
+      color[u] = 'black'; //完成探索改顶点和其相邻顶点后，将该顶点标注为已探索过的。
       if(callback){
         callback(u);
       }
     }
   }
+  //寻找最短路径
+  this.BFS = function (v) {
+    var color = initializeColor(),
+      queue = new Queue(),
+      d = [],
+      pred = [];
+    queue.enqueue(v);
+
+    for(var i = 0; i < vertices.length; i++){
+      d[vertices[i]] = 0;
+      pred[vertices[i]] = null;
+    }
+    while (!queue.isEmpty()){
+      var u = queue.dequeue(),
+        neighbors = adjList.get(u);
+      color[u] = 'grey';
+      for(var i = 0; i < neighbors.length; i++){
+        var w = neighbors[i];
+        if(color[w] === 'white'){
+          color[w] = 'grey';
+          d[w] = d[u] + 1; //先寻找距离为1的，然后寻找距离为2的，以此类推
+          pred[w] = u;
+          queue.enqueue(w);
+        }
+      }
+      color[u] = 'black';
+    }
+  }
 }
+var graph = new Graph();
+var myVertices = ['A','B','C','D','E','F','G','H','I'];
+for(var i = 0; i < myVertices.length; i++){
+  graph.addVertex(myVertices[i]);
+}
+graph.addEdge('A', 'B');
+graph.addEdge('A', 'C');
+graph.addEdge('A', 'D');
+graph.addEdge('C', 'D');
+graph.addEdge('C', 'G');
+graph.addEdge('D', 'G');
+graph.addEdge('D', 'H');
+graph.addEdge('B', 'E');
+graph.addEdge('B', 'F');
+graph.addEdge('E', 'I');
+
