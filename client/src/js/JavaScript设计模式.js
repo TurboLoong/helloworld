8e51ed88b387395cc80e04df95fc564f0897a48e
TurboLoong <turboloong@gmail.com>
2017-08-09 07:52:00 +0800

    组合模式

--- client/src/js/JavaScript设计模式.js
+++ client/src/js/JavaScript设计模式.js
@@ -538,6 +538,7 @@ var iteratorUploadObj = function(){
 var uploadObj = iteratorUploadObj( getActiveUploadObj, getWebkitUploadObj,
     getFlashUploadObj, getHtml5UploadObj, getFormUpladObj )
 
+
 /**
  *@author: TurboLoong
  *@date: 2017/7/27
@@ -554,3 +555,94 @@ Function.prototype.after = function( fn ){
         return ret;
     }
 };
+
+//var order = order500.after(order200).after(orderNormal)
+
+/**
+ *@author: TurboLoong
+ *@date: 2017/8/7
+ *@des: 中介者模式
+ */
+
+//购买商品
+
+/**
+ *@author: TurboLoong
+ *@date: 2017/8/9
+ *@des: 组合模式
+ * 表示对象的部分整体层次结构。
+ * 客户希望统一对待树中的所有对象。
+ */
+var MacroCommand = function(){
+    return {
+        commandsList: [],
+        add: function( command ){
+            this.commandsList.push( command );
+        },
+        execute: function(){
+            for ( var i = 0, command; command = this.commandsList[ i++ ]; ){
+                command.execute();
+            }
+        }
+    }
+};
+var openAcCommand = {
+    execute: function(){
+        console.log( '打开空调' );
+    }
+};
+/**********家里的电视和音响是连接在一起的，所以可以用一个宏命令来组合打开电视和打开音响的命令
+ *********/
+var openTvCommand = {
+    execute: function(){
+        console.log( '打开电视' );
+    }
+};
+var openSoundCommand = {
+    execute: function(){
+        console.log( '打开音响' );
+    }
+};
+var macroCommand1 = MacroCommand();
+macroCommand1.add( openTvCommand );
+macroCommand1.add( openSoundCommand );
+/*********关门、打开电脑和打登录 QQ 的命令****************/
+var closeDoorCommand = {
+    execute: function(){
+        console.log( '关门' );
+    }
+};
+var openPcCommand = {
+    execute: function(){
+        console.log( '开电脑' );
+    }
+};
+var openQQCommand = {
+    execute: function(){
+        console.log( '登录 QQ' );
+    }
+};
+var macroCommand2 = MacroCommand();
+macroCommand2.add( closeDoorCommand );
+macroCommand2.add( openPcCommand );
+macroCommand2.add( openQQCommand );
+/*********现在把所有的命令组合成一个“超级命令”**********/
+var macroCommand = MacroCommand();
+macroCommand.add( openAcCommand );
+macroCommand.add( macroCommand1 );
+macroCommand.add( macroCommand2 );
+/*********最后给遥控器绑定“超级命令”**********/
+var setCommand = (function( command ){
+    document.getElementById( 'button' ).onclick = function(){
+        command.execute();
+    }
+})( macroCommand );
+
+// 打开空调
+// 打开电视
+// 打开音响
+// 关门
+// 开电脑
+// 登录QQ
+
+//
