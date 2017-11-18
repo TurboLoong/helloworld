a27961f2f0f7f41a8c6445b83b3b02a8f41d25b6
turboloong <xueyinglong@bbdservice.com>
2017-11-18 17:33:28 +0800

    1.webpack 启动

--- client/index.js
+++ client/index.js
@@ -6,30 +6,51 @@
 import zrender from 'zrender';
 var zr = zrender.init(document.getElementById('main'));
 zr.clear();
-for (var i = 1; i < 7; i++) {
-    zr.add(new zrender.Circle({
-        shape: {
-            cx: 250 + 50 * Math.cos(2*Math.PI*(i - 1) / 6).toFixed(2),
-            cy: 250 + 50 * Math.sin(2*Math.PI*(i - 1) / 6).toFixed(2),
-            r: 5
-        },
-        style: {
-            fill: 'none',
-            stroke: '#F00'
+
+function render(r, n, count) {
+    let m = 1;
+    for (var i = 0; i < n; i++) {
+        m++;
+        zr.add(new zrender.Circle({
+            shape: {
+                cx: 250 + r * Math.cos(2*Math.PI*(i) / n).toFixed(2),
+                cy: 250 + r * Math.sin(2*Math.PI*(i) / n).toFixed(2),
+                r: 5
+            },
+            style: {
+                fill: 'none',
+                stroke: '#F00'
+            }
+        }));
+
+        if(count == m){
+            break;
         }
-    }));
+    }
+}
+let defaultR = 60, defaultCount = 12, d = 20;
+let count = 0;
+let m = 0; //第几圈
+const total = 317;
+let currentR = defaultR, currentCount = defaultCount;
+let lastCount = 0;
+while (count < total)
+{
+    lastCount = count;
+    let leftCount = total - lastCount;
+    render(currentR, currentCount, leftCount);
+    currentR = defaultR + m * d;
+    currentCount = defaultCount*currentR/defaultR;
+    m++;
+    count += currentCount;
 }
 
-for (var i = 1; i < 13; i++) {
-    zr.add(new zrender.Circle({
-        shape: {
-            cx: 250 + 80 * Math.cos(2*Math.PI*(i - 1) / 12).toFixed(2),
-            cy: 250 + 80 * Math.sin(2*Math.PI*(i - 1) / 12).toFixed(2),
-            r: 5
-        },
-        style: {
-            fill: 'none',
-            stroke: '#F00'
-        }
-    }));
-}
+
+
+
+
+
+
+
+
+
