06770dfa6f7187ea4cce27661dc5f051277a5fff
TurboLoong <2312987772@qq.com>
2019-04-22 10:10:28 +0800

    合并

--- client/index.js
+++ client/index.js
@@ -7,8 +7,4 @@ import './index.scss';
 // import './demo/zrender';
 // import './demo/drawImage';
 // import './demo/gaode';
-<<<<<<< HEAD
-// import './demo/d3';
-=======
 import './demo/d3/d3';
->>>>>>> ee726714f363fa4e9cf211783954c35073c15bf7
